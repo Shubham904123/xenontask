a281ac1e13867dcda912d889370bbd7e58e12e95
Shubham904123 <shubham9041231497@gmail.com>
2023-03-07 19:46:52 +0530

    first commit

---
+++ src/database/conn.js
@@ -0,0 +1,22 @@
+// const mongoose = require('mongoose');
+// mongoose.connect("mongodb://localhost:27017/project" , {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// }).then(() =>{
+//     console.log("connection successfull");
+// }).catch((error) => {
+//     console.log(error);
+// })
+
+
+const db = 'mongodb+srv://admin:admin@cluster0.fe8izhn.mongodb.net/?retryWrites=true&w=majority';
+
+const mongoose = require('mongoose');
+mongoose.connect(db , {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() =>{
+    console.log("connection successfull");
+}).catch((error) => {
+    console.log(error);
+})
