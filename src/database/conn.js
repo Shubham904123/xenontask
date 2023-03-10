9207ddf6fe764ee3924bd2f7288b1e6868e80ffb
Shubham904123 <54353371+Shubham904123@users.noreply.github.com>
2023-03-10 12:48:20 +0530

    Update conn.js

--- src/database/conn.js
+++ src/database/conn.js
@@ -9,7 +9,7 @@
 // })
 
 
-const db = 'mongodb+srv://admin:shubham123@cluster0.fe8izhn.mongodb.net/xenonstack?retryWrites=true&w=majority';
+const db = 'mongodb+srv://shubham123:shubham123@cluster0.fe8izhn.mongodb.net/?retryWrites=true&w=majority';
 
 const mongoose = require('mongoose');
 mongoose.connect(db , {
